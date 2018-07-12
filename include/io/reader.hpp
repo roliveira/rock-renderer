e0909f1850dc31dfece5cef12c20687de7413fef
Rodolfo Oliveira <oliveira.rodolfo87@gmail.com>
2018-07-12 16:17:40 +0100

    initial sketch for the renderer
    I should leave the functions as a standalone include header instead of keeping it all inside volume.cpp.

---
+++ include/io/reader.hpp
@@ -0,0 +1,97 @@
+#ifndef IO_READER_HPP
+#define IO_READER_HPP
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+
+
+
+std::vector<std::string> String2Tokens(std::string istr)
+{
+    std::stringstream ss(istr);
+    std::string buf;
+    std::vector<std::string> tokens;
+
+    while (ss >> buf) 
+    {
+        tokens.push_back(buf);
+    }
+
+    return tokens;
+}
+
+inline std::unordered_map<std::string, float> ReadASCIIHeader(const char* fname)
+{
+    std::unordered_map<std::string, float> out;
+
+    std::string temp_str;
+    std::vector<std::string> temp_tokens;
+
+    std::ifstream ifs(fname);
+
+    std::getline(ifs, temp_str);  // TODO: proper handle if it's not ASCII
+    std::getline(ifs, temp_str);  // TODO: double check if this is the type of the data
+
+    std::getline(ifs, temp_str);
+    temp_tokens = String2Tokens(temp_str);
+    
+    out["nx"] = static_cast<float>(std::atoi(temp_tokens.at(0).c_str()));
+    out["ny"] = static_cast<float>(std::atoi(temp_tokens.at(1).c_str()));
+    out["nz"] = static_cast<float>(std::atoi(temp_tokens.at(2).c_str()));
+
+    std::getline(ifs, temp_str);
+    temp_tokens = String2Tokens(temp_str);
+
+    out["ox"] = std::atof(temp_tokens.at(0).c_str());
+    out["lx"] = std::atof(temp_tokens.at(1).c_str());
+    out["oy"] = std::atof(temp_tokens.at(2).c_str());
+    out["ly"] = std::atof(temp_tokens.at(3).c_str());
+    out["oz"] = std::atof(temp_tokens.at(4).c_str());
+    out["lz"] = std::atof(temp_tokens.at(5).c_str());
+    
+    ifs.close();
+
+    return out;
+}
+
+inline std::vector<unsigned char> ReadASCII(const char* fname, int n)
+{
+    std::vector<unsigned char> out(n);
+    int nx, ny, nz;
+
+    std::string temp_str;
+    std::vector<std::string> temp_tokens;
+
+    std::ifstream ifs(fname);
+
+    std::getline(ifs, temp_str);
+    
+    for(int i=0; i<3; ++i) 
+    {
+        std::getline(ifs, temp_str);
+    }
+    
+    int idata = 0;
+
+    while (std::getline(ifs, temp_str))
+    {
+        temp_tokens = String2Tokens(temp_str);
+        
+        if (temp_tokens.size() == 0) continue;
+
+        for (std::string istr : temp_tokens)
+        {
+            out[idata++] = std::atoi(istr.c_str())*255;
+        }
+    }
+    
+    ifs.close();
+
+    return out;
+}
+
+
+#endif // IO_READER_HPP
---
+++ modules/volume.cpp
@@ -0,0 +1,145 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <unordered_map>
+
+#include "ospray/ospray.h"
+#include "ospray/ospcommon/vec.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include "io/reader.hpp"
+
+
+using namespace ospcommon;
+
+
+
+void setup_volume(OSPVolume volume, std::vector<unsigned char> volume_data, OSPTransferFunction transfer_fcn, const vec3i &dims) {
+	const std::vector<vec3f> colors = {
+		vec3f(0, 0, 0),
+		vec3f(0.9, 0.9, 0.9)
+	};
+
+	const std::vector<float> opacities = {0.1f, 0.5f};
+	OSPData colors_data = ospNewData(colors.size(), OSP_FLOAT3, colors.data());
+	ospCommit(colors_data);
+	OSPData opacity_data = ospNewData(opacities.size(), OSP_FLOAT, opacities.data());
+	ospCommit(opacity_data);
+
+	const vec2f value_range(static_cast<float>(0), static_cast<float>(255));
+	ospSetData(transfer_fcn, "colors", colors_data);
+	ospSetData(transfer_fcn, "opacities", opacity_data);
+	ospSetVec2f(transfer_fcn, "valueRange", (osp::vec2f&)value_range);
+	ospCommit(transfer_fcn);
+
+	// std::vector<unsigned char> volume_data(dims.x * dims.y * dims.z, 0);
+	// for (size_t i = 0; i < volume_data.size(); ++i) {
+	// 	volume_data[i] = (i / (dims.x * dims.y)) * 255;
+	// }
+
+	// TODO: Here you can also enable gradient shading and volume lighting if you'd like
+
+	ospSetString(volume, "voxelType", "uchar");
+	ospSetVec3i(volume, "dimensions", (osp::vec3i&)dims);
+	ospSetObject(volume, "transferFunction", transfer_fcn);
+	// This will copy the volume data into OSPRay's volume where it'll re-organize
+	// it for better memory access
+	ospSetRegion(volume, volume_data.data(), osp::vec3i{0, 0, 0}, (osp::vec3i&)dims);
+	ospCommit(volume);
+}
+
+
+
+int main(int argc, const char **argv) 
+{
+    if (argc < 3)
+    {
+        std::cerr <<  "Usage: " << argv[0] << " INFILE OUTFILE" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    ospInit(&argc, argv);
+
+    std::string ifname = argv[1];
+    std::string ofname = argv[2];
+
+    std::unordered_map<std::string, float> header = ReadASCIIHeader(ifname.c_str());
+    std::vector<unsigned char> volume_data = ReadASCII(ifname.c_str(), header["nx"]*header["ny"]*header["nz"]);
+
+    const vec3i volume_dims(header["nx"], header["ny"], header["nz"]);
+	OSPTransferFunction transfer_fcn = ospNewTransferFunction("piecewise_linear");
+
+	OSPVolume volume = ospNewVolume("block_bricked_volume");
+	ospSetObject(volume, "transferFunction", transfer_fcn);
+
+	const vec2i img_size(1024, 1024);
+	const vec3f cam_pos(-volume_dims[0], -volume_dims[1], 1.25*volume_dims[2]);
+	const vec3f cam_dir = vec3f(volume_dims) / 2.f - cam_pos;
+	const vec3f cam_up(0, 0, -1);
+
+	// Setup the camera to render from
+	OSPCamera camera = ospNewCamera("perspective");
+	ospSet1f(camera, "aspect", img_size.x / static_cast<float>(img_size.y));
+	ospSetVec3f(camera, "pos", (osp::vec3f&)cam_pos);
+	ospSetVec3f(camera, "dir", (osp::vec3f&)cam_dir);
+	ospSetVec3f(camera, "up", (osp::vec3f&)cam_up);
+	ospCommit(camera);
+
+	setup_volume(volume, volume_data, transfer_fcn, volume_dims);
+
+    // Create the renderer we'll use to render the image
+	OSPRenderer renderer = ospNewRenderer("scivis");
+
+	OSPModel model = ospNewModel();
+	ospAddVolume(model, volume);
+	ospCommit(model);
+
+
+    // Create an ambient light, which will be used to compute ambient occlusion
+	OSPLight ambient_light = ospNewLight(renderer, "ambient");
+	ospSet1f(ambient_light, "intensity", 0.75);
+	ospCommit(ambient_light);
+
+    OSPLight sun_light = ospNewLight(renderer, "distant");
+	ospSetVec3f(sun_light, "direction", osp::vec3f{1.f, 1.f, 0.5f});
+	ospSetVec3f(sun_light, "color", osp::vec3f{1.f, 1.f, 0.8f});
+	ospSet1f(sun_light, "intensity", 0.8);
+	ospSet1f(sun_light, "angularDiameter", 1);
+	ospCommit(sun_light);
+
+	OSPLight fill_light = ospNewLight(renderer, "distant");
+	ospSetVec3f(fill_light, "direction", osp::vec3f{0.5f, 1.f, 1.5f});
+	ospSetVec3f(fill_light, "color", osp::vec3f{1.f, 1.f, 0.8f});
+	ospSet1f(fill_light, "intensity", 0.2);
+	ospSet1f(fill_light, "angularDiameter", 8);
+	ospCommit(fill_light);
+
+	std::vector<OSPLight> lights_list = { ambient_light, sun_light, fill_light };
+	OSPData lights = ospNewData(lights_list.size(), OSP_LIGHT, lights_list.data(), 0);
+	ospCommit(lights);
+
+    // Setup other renderer params
+	ospSetObject(renderer, "model", model);
+	ospSetObject(renderer, "camera", camera);
+	ospSetObject(renderer, "lights", lights);
+	ospSet1i(renderer, "shadowsEnabled", 1);
+	ospSet1i(renderer, "aoSamples", 8);
+	ospSet1i(renderer, "spp", 16);
+	ospSetVec4f(renderer, "bgColor", osp::vec4f{0.0f, 0.0f, 0.0f, 0.0f});
+	ospCommit(renderer);
+
+	OSPFrameBuffer framebuffer = ospNewFrameBuffer((osp::vec2i&)img_size, OSP_FB_SRGBA, OSP_FB_COLOR);
+	ospFrameBufferClear(framebuffer, OSP_FB_COLOR);
+
+	// Finally, render the image and save it out
+	ospRenderFrame(framebuffer, renderer, OSP_FB_COLOR);
+
+	const uint32_t *fb = static_cast<const uint32_t*>(ospMapFrameBuffer(framebuffer, OSP_FB_COLOR));
+	stbi_write_png(ofname.c_str(), img_size.x, img_size.y, 4, fb, img_size.x * sizeof(uint32_t));
+	ospUnmapFrameBuffer(fb, framebuffer);
+
+    return EXIT_SUCCESS;
+}
